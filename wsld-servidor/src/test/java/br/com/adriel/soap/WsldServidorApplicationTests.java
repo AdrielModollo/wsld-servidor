d366b87f1eae1da4e507810937090f0fe2373c4f
Adriel <adrielmodollo@outlook.com>
2021-09-23 22:29:29 -0300

    SOAP inicial

---
+++ wsld-servidor/src/test/java/br/com/adriel/soap/WsldServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adriel.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsldServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
