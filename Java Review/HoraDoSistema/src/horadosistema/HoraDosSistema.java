1225a823ad2e4a8d42ebaa999c3bc024253bf81f
Adriel <adrielmodollo@outlook.com>
2021-09-28 20:51:31 -0300

    Hora do sistema/Primeiro programa

---
+++ Java Review/HoraDoSistema/src/horadosistema/HoraDosSistema.java
@@ -0,0 +1,12 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDosSistema {
+	
+	public static void main(String[] args) {
+		Date relogio = new Date(); //New cria um novo objeto
+		System.out.println("A hora do sistema é");
+		System.out.println(relogio.toString());
+	}
+}
